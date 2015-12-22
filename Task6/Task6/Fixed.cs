55d8c5b6bf1567525f7bdfdd46c114409f98bb3c
Maryna Kovalenko <maryna_kovalenko@live.com>
2015-12-22 18:32:55 +0200

    task6

---
+++ Task6/Task6/Fixed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class FixedPayEmployee : Employee
+    {
+
+        public FixedPayEmployee(string name, string secondName, int id, double rate)
+            : base(name, secondName, id, rate)
+        {
+        }
+         public override double calculateSallary()
+        {
+            double averageMonthSallary = Rate;
+            return averageMonthSallary;
+        }
+    }
+
+}
---
+++ Task6/Task6/Hourly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class HourlyEmployee : Employee
+    {
+        public HourlyEmployee(string name, string secondName, int id, double rate) : base(name, secondName, id, rate)
+        {            
+        }
+
+         public override double calculateSallary()
+        {
+            double averageMonthSallary = Rate * 20.8 * 8;
+            return averageMonthSallary;
+        }
+    }
+}
