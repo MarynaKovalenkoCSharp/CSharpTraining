e39cc1b7e5c35e53ea6a9b7e33435a7ffc278414
Maryna Kovalenko <marina.a.kovalenko@gmail.com>
2016-01-21 12:53:32 +0200

    Add all this shit back

---
+++ Hometask2/Hometask2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hometask2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            switch (today) {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Понедельник - день тяжелый");
+                        break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Вторник тоже день нелегкий");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Ура, уже среда! Прошла почти половина рабочей недели");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Сегодня четверг!");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Наконец-то пятница!");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Поздравляем! Сегодня суббота!");
+                    break;
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Сегодня воскресенье, завтра на работу");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Hometasks/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hometasks
+{
+    class Execution
+    {
+        static void Main(string[] args)
+        {
+            //For Hometask1
+            OverloadedFunctions example = new OverloadedFunctions();
+            example.Func("string");
+            example.Func(-1);
+
+            //For Hometask2
+            MessageForEachDay.writeMessage();
+
+            //For Hometask3
+            TodaysDay.splitData();
+            Console.ReadKey();
+        }
+    }
+}
