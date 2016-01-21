48995f0936ef6df80d3ccd35af0e9378235a27ab
Maryna Kovalenko <marina.a.kovalenko@gmail.com>
2016-01-21 13:16:07 +0200

    ADD

---
+++ Task6/Task6/Fixed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class FixedPayEmployee : Employee
+    {
+        public FixedPayEmployee(string name, string secondName, int id, int rate)
+            : base(name, secondName, id, rate)
+        {
+        }
+
+        public override double CalculateSallary()
+        {
+            double averageMonthSallary = Rate;
+            return averageMonthSallary;
+        }
+    }
+}
